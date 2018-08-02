c9cf2d6111d9d71a98ef20aa0d3617d35f6c5bdd
bruno.sugahara <bbbbruno69@gmail.com>
2018-08-02 19:19:41 +0900

    Add new

--- src/controllers/New.java
+++ src/controllers/New.java
@@ -2,11 +2,14 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
 
 /**
  * Servlet implementation class New
@@ -27,8 +30,11 @@ public class New extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("tasks", new Tasks());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/new.jsp");
+        rd.forward(request, response);
 	}
 
 }
